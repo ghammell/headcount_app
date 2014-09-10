934e555ec1a0bc82251f38a36d4af88cba004034
NokoGary 3 JF <ghammell1@gmail.com>
2014-09-10 10:20:23 -0700

    finalized scroll 4544 based off input

--- public/js/application.js
+++ public/js/application.js
@@ -19,41 +19,16 @@ $(document).ready(function() {
     })
   })
 
-  // NEW STRATEGY: MAKE ONE BUTTON FOR REFRESH, WHICH UPDATES THE VALUES FOR ALL INPUTS IN THE FORM
-  // GIVE EACH NEW SECTION A UNIQUE ID, THIS WAY WHEN YOU RESET THE VALUES
-  // YOU KNOW WHICH IDS TO SET WHICH VALUES TO
-
-  $(".scroll_container").on("click", ".refresh", function(event){
-    event.preventDefault()
-    form_data = $(this).parent().serialize()
-    $.ajax({
-      url: '/refresh',
-      method: 'get',
-      data: form_data
-    })
-    .done( function(data) {
-      console.log(typeof data)
-      console.log(data["salary_input"])
-      $(this).siblings(".salary_input").attr("value", data["salary_input"])
-    })
-    .fail( function() {
-      console.log("fail")
-    })
-  })
-
-
-
-
   $(function add_scroll_effect() {
-    var budgeted_salary = $(".salary_input").attr("value")
     var counting_object = $("span.salary_calc")
+    var total_width = parseInt($("#test_bar").css("width").replace("px", "")) - parseInt($("#test_scroll").css("width").replace("px", ""))
 
     $( "#test_scroll" ).draggable({
       containment: "parent",
       opacity: 0.5,
       drag: function(e) {
+        var budgeted_salary = $(".salary_input").val()
         css_left_px = parseInt($("#test_scroll").css("left").replace("px", ""))
-        total_width = parseInt($("#test_bar").css("width").replace("px", "")) - parseInt($("#test_scroll").css("width").replace("px", ""))
         dollar_value = css_left_px / total_width * budgeted_salary
         counting_object.text("$" + dollar_value.toFixed());
       }
