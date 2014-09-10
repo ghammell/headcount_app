07840ab971c46b64e49dfa8a9c70762c5b7eee26
NokoGary 3 JF <ghammell1@gmail.com>
2014-09-10 10:41:11 -0700

    added start and end functionality:

--- public/js/application.js
+++ public/js/application.js
@@ -21,17 +21,39 @@ $(document).ready(function() {
 
   $(function add_scroll_effect() {
     var counting_object = $("span.salary_calc")
-    var total_width = parseInt($("#test_bar").css("width").replace("px", "")) - parseInt($("#test_scroll").css("width").replace("px", ""))
+    var total_width = parseInt($("#test_bar").css("width").replace("px", "")) - parseInt($("#test_scroll_end").css("width").replace("px", ""))
 
-    $( "#test_scroll" ).draggable({
+    $( "#test_scroll_end" ).draggable({
       containment: "parent",
       opacity: 0.5,
       drag: function(e) {
         var budgeted_salary = $(".salary_input").val()
-        css_left_px = parseInt($("#test_scroll").css("left").replace("px", ""))
-        dollar_value = css_left_px / total_width * budgeted_salary
+        css_left_px_end = parseInt($("#test_scroll_end").css("left").replace("px", ""))
+        css_left_px_start = parseInt($("#test_scroll_start").css("left").replace("px", ""))
+        dollar_value = (css_left_px_end - css_left_px_start) / total_width * budgeted_salary
         counting_object.text("$" + dollar_value.toFixed());
       }
     });
   });
+
+  $(function add_scroll_effect() {
+    var counting_object = $("span.salary_calc")
+    var total_width = parseInt($("#test_bar").css("width").replace("px", "")) - parseInt($("#test_scroll_start").css("width").replace("px", ""))
+
+    $( "#test_scroll_start" ).draggable({
+      containment: "parent",
+      opacity: 0.5,
+      drag: function(e) {
+        var budgeted_salary = $(".salary_input").val()
+        css_left_px_end = parseInt($("#test_scroll_end").css("left").replace("px", ""))
+        css_left_px_start = parseInt($("#test_scroll_start").css("left").replace("px", ""))
+        dollar_value = (css_left_px_end - css_left_px_start) / total_width * budgeted_salary
+        counting_object.text("$" + dollar_value.toFixed());
+      }
+    });
+  });
+
+
+
+
 });
