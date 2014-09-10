f95f2243f745b51febd23409b68b04874e393c15
NokoGary 3 JF <ghammell1@gmail.com>
2014-09-09 23:58:18 -0700

    can now increment based on movement

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#add_scroll_bar_button").on("click", function(event){
+    event.preventDefault()
+    $.ajax({
+      url: '/new_bar',
+      method: 'get'
+    })
+    .done( function(data) {
+      $(".scroll_container").append(data)
+      $(".scroll").each( function(index, value) {
+        $(value).draggable({
+          containment: $(value).parent()
+        })
+      })
+    })
+    .fail( function() {
+      console.log("fail")
+    })
+  })
+
+  $(function add_scroll_effect() {
+    var count = 0;
+    var budgeted_salary = $(".salary_input")
+    var prevX = -1;
+    var counting_object = $("span.salary_calc")
+
+    $( "#test_scroll" ).draggable({
+      containment: "parent",
+      opacity: 0.5,
+      drag: function(e) {
+        counting_object.text($("#test_scroll").css("left"));
+      }
+    });
+  });
 });
